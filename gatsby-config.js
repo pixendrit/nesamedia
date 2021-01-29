2ef92e374e951000f61e818e5c2a067505b1d11a
pixendrit <pixendrit@outlook.com>
2021-01-29 11:23:29 +0100

    try again

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,12 +43,12 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `static/android-chrome.png`,
+            src: `/android-chrome.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `static/android-chrome512.png`,
+            src: `/android-chrome512.png`,
             sizes: `512x512`,
             type: `image/png`,
           },
